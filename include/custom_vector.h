336294ed4eb746f829f211aca8d91cfbfa68b56b
W-Jeng <woonjeng@gmail.com>
2024-12-18 21:32:36 +0800

    custom vector operator check

--- include/custom_vector.h
+++ include/custom_vector.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include "custom_allocator.h"
+#include <memory>
 
 namespace ctm 
 {
@@ -6,7 +9,88 @@ namespace ctm
 template <typename T, typename Alloc = ctm::allocator<T>>
 class vector 
 {
+private:
+    T* data_;
+    std::size_t size_;  
+    std::size_t capacity_;
+    Alloc allocator;
+
+public:
+    vector(): data_(nullptr),
+        size_(0),
+        capacity_(0) {}
+
+    ~vector()
+    {
+        for (size_t i = 0; i < size_; ++i)
+        {
+            allocator.destroy(data_ + i);
+        }
+        if (data_)
+        {
+            allocator.deallocate(data_, capacity_);
+        }
+        return;
+    }
     
+    void push_back(const T& value)
+    {
+        if (size_ == capacity_)
+        {
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+        }
+        allocator.construct(data_ + size_, value);
+        ++size_;
+        return;
+    }
+
+    void reserve(std::size_t new_capacity)
+    {
+        if (new_capacity <= capacity_)
+        {
+            return;
+        }
+
+        T* new_data = allocator.allocate(new_capacity);
+
+        for (std::size_t i = 0; i < size_; ++i)
+        {
+            allocator.construct(new_data+i, std::move(data_[i]));
+            allocator.destroy(data_+i);
+        }
+
+        if (data_)
+        {
+            allocator.deallocate(data_, capacity_);
+        }
+
+        data_ = new_data;
+        capacity_ = new_capacity;
+        return;
+    }
+
+    // operators
+    const T& at(std::size_t index) const
+    {
+        if (index < 0 || index >= size_)
+        {
+            throw std::out_of_range("Indexing out of range");
+        }
+        return data_[index];
+    }
+
+    // accessed when using const vector
+    const T& operator[](std::size_t index) const
+    {
+        return data_[index];
+    }
+
+    T& operator[](std::size_t index)
+    {
+        return data_[index];
+    }
+
+
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,33 @@
 #include "custom_allocator.h"
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+
+
+void test_const(const ctm::vector<int>& vec)
+{
+    std::cout << "testing const, vec 0: " << vec[0] << std::endl;
+}
 
 int main()
 {
     std::cout << "hello world!" << std::endl;
-    ctm::allocator<int> alloc;
-
-    int* b = alloc.allocate(5);
-
-    for (std::size_t i = 0; i < 5; ++i)
-    {
-        alloc.construct(&b[i], static_cast<int>(i * 10));
-    }
-
-    for (std::size_t i = 0; i < 5; ++i)
-    {
-        std::cout << "b[" << i << "] = " << b[i] << "\n";
-    }
-
-    for (std::size_t i = 0; i < 5; ++i)
-    {
-        alloc.destroy(&b[i]);
-    }
-
-    alloc.deallocate(b, 5);
-
     ctm::vector<int> temp;
+    temp.push_back(1);
+    temp[0] = 2;
+    std::cout << "temp 0: " << temp[0] << std::endl;
+
+    int x = temp[0];
+
+    test_const(temp);
+
+    temp[1] = 1;
+    std::cout << "temp 1: " << temp[1] << std::endl;
+
+    std::vector<int> std_vec;
+    std_vec.push_back(1);
+    std_vec[100] = 2;
+    std::cout << "std vec: " << std_vec[100] << std::endl;
 
     return EXIT_SUCCESS;
 }
